e91c7bc2ee3764d5b575da491e166639b7f51ec9
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-09-12 22:25:39 +0530

    [ADDED]: shubham this code for print Area of square

---
+++ AreaOfSquare.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace::std;
+int main(){
+	
+	//find Area of a square
+	
+	int a=4;
+	int area=0;
+	
+	area=a*a;
+	
+	cout<<"Area of square:="<<area;
+	
+	return 0;
+}
