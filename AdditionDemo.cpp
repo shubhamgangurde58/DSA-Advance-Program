485a81021f3ff6ad26783071d994b9aa3655a25c
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-09-12 22:24:29 +0530

    [ADDED]: shubham this code for Print Addition of 2 number program

---
+++ AdditionDemo.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace::std;
+int main(){
+	
+	// Addition of 2 number a & b;
+	
+	int a=10;
+	int b=20;
+	int c=0;
+	
+	c=a+b;
+	
+	cout<<"\nAddition="<<c;
+	
+	return 0;
+}
