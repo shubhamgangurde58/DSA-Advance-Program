b780aded9a5233dcdf98e806a22a78e97dbbfe40
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-09-12 22:23:18 +0530

    [ADDED]: shubham This code for primative datatype

---
+++ DataTypeDemo.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace::std;
+int main(){
+	
+	//Basic example of  primative data type ;
+	
+	// integer datatype
+	int a=10;
+	cout<<"Value of integer variable:= "<<a<<endl;
+	cout<<"Size of integer data type in byte:= "<<sizeof(a)<<endl<<endl;
+	
+	//float datatype
+	float b=12.7;
+	cout<<"value of float variable:= "<<b<<endl;
+	cout<<"Size of float data type in byte:= "<<sizeof(b)<<endl<<endl;
+	
+	//character datatype
+	char c='s';
+	cout<<"value of character variable :="<<c<<endl;
+	cout<<"Size of character datatype in byte:="<<sizeof(c)<<endl<<endl;
+	
+	//Boolean datatype
+	bool d=true;
+	cout<<"value of boolean variable :="<<d<<endl;
+	cout<<"Size of boolean datatype in byte :="<<sizeof(d)<<endl<<endl;
+	
+	//double datatype
+	double e=10.25;
+	cout<<"value of double variable :="<<e<<endl;
+	cout<<"Size of double datatype in byte :="<<sizeof(e)<<endl<<endl;
+	
+	return 0;
+}
