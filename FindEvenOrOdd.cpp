f06bd3149bc0afac5052c47349a702b9af29186f
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-09-13 21:21:02 +0530

    [ADD]

---
+++ FindEvenOrOdd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace::std;
+int main(){
+	
+	//find the given number is even or odd ;
+	
+	int n=8;
+	
+	if(n%2==0){
+		cout<<" "<<n<<" Number is Even";
+	}else{
+		cout<<" "<<n<<" Number is Odd";
+	}
+
+	return 0;
+}
