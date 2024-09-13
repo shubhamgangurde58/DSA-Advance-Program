16be3a3bd7f2e60811304e86449db8be3fe77eb5
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-09-13 21:20:36 +0530

    [ADDED]: shubham This code for Find maximum number

---
+++ FindMaximumNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace::std;
+int main(){
+	
+	//find maximum number of 2 number ;
+	
+	int a=4;
+	int b=3;
+	
+	if(a>b){
+		cout<<"Max number is A:="<<a;
+	}else{
+		cout<<"Max number is B:="<<b;
+	}
+	
+	return 0;
+}
