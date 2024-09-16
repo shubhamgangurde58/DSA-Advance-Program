014f9773975a39703b5672e3bed3879c2b068636
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-09-16 19:22:40 +0530

    [ADDED]: shubham This code for find simple interest

---
+++ FindSimpleInterest.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace::std;
+int main(){
+	
+	//Calculate " Simple Interest" from principal(p) , Rate(r) , Time (t) ;
+	
+	int p=2000;
+	int r=5;
+	int t=2;
+	
+	cout<<"Simple Interest="<<(p*r*t)/100<<endl;
+	
+	return 0;
+}
