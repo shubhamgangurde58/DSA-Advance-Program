1347a6f582ac3b5ddbd871c12877c4dcdb5cebf5
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-09-16 19:18:13 +0530

    [ADDED]: shubham This code for Find Minimum number

---
+++ FindMinimumNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace::std;
+int main(){
+	
+	//find min number of 2 number ;
+	
+	int a=4;
+	int b=3;
+	
+	if(a<b){
+		cout<<"Min number is A:="<<a;
+	}else{
+		cout<<"Min number is B:="<<b;
+	}
+	
+	return 0;
+}
