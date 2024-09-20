7cfdb5ccfe4fd381e289a9aa34452ba11eef5a70
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-09-20 18:34:34 +0530

    [ADDED]: shubham This code for print all Operator Demo

---
+++ OperatorsDemo.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace::std;
+int main(){
+	
+	//Basic example in type of operators 
+	
+	// 1. Arithmatic operator 
+	// Addition +, subtraction -, multiple *, division / , modules %
+	
+	int a=25;
+	int b=10;
+	
+	cout<<"Addition:="<<(a+b)<<endl;
+	cout<<"Subtraction:="<<(a-b)<<endl;
+	cout<<"Multiple:="<<(a*b)<<endl;
+	cout<<"Division:="<<(a/b)<<endl;
+	cout<<"Remainder:="<<(a%b==0)<<endl<<endl<<endl;
+	
+	//2. Logical operator
+	// equalTo =, double equalTo ==, greater than >, less than <,
+	// greaterthanEqualTo >=,lessThanEqualTo <= notEqualTo !=;
+	
+	int x=5;
+	int y=3;
+	
+	cout<<"EqualTo :="<<(x=10)<<endl;
+	cout<<"Double equalTo :="<<(a==b)<<endl;
+	cout<<"Greater than :="<<
+	return 0;
+}
