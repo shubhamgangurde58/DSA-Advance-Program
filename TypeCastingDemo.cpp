8fe30d67a1c7a31cd775f183bb4e1f51f20d214f
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-09-21 21:25:11 +0530

    [ADDED]: shubham This code for print type casting code

---
+++ TypeCastingDemo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace::std;
+int main(){
+	
+	//Basic example of type conversion & type casting;
+	
+	char grade='z';	
+	int value = grade;
+		 
+	cout<<value<<endl;
+	 	 	 	 
+	return 0;
+} 
