e729e348771ebcd2bf38d4ff232a160109cddbdb
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-09-22 16:04:49 +0530

    [ADDED]: shubham This code for Print for loop example

---
+++ ForLoopDemo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace::std;
+int main(){
+	
+	// for loop example
+	
+	int n=20;
+	
+	for(int i=0;i<=n;i++){
+	
+		if(i>=10){
+			
+			break;
+		}	
+		cout<<i<<endl;
+		
+	}
+	return 0;
+}
