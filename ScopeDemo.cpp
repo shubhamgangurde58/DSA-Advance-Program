debc16909e62a1bbb25f93dfbc29d7237f164fa1
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-10-03 21:13:13 +0530

    [ADDED]: shubham This code for Print Globle variable Demo

---
+++ ScopeDemo.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace::std;
+
+int i=10;
+
+void Demo(){
+	cout<<i<<endl;
+}
+
+int main(){
+	
+	for(int i=0;i<10;i++){
+		//cout<<i<<endl;
+	}
+	
+	Demo();
+	
+	cout<<i<<endl;
+	
+}
